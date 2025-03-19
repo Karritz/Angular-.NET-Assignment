f5356bc4223dbedc7503e09220ce193560c41229
Gareth Williams <65289470+Karritz@users.noreply.github.com>
2025-03-19 16:22:08 +0200

    Removed unused stuff

--- AngularApp/angularapp.client/src/app/services/http.service.ts
+++ AngularApp/angularapp.client/src/app/services/http.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { DataSource } from 'devextreme/common/data';
 
 @Injectable({
   providedIn: 'root'
@@ -11,21 +10,5 @@ export class HttpService {
 
   getRandomData() {
     return this.httpClient.get("https://localhost:7076/RandomData")
-  }
-
-  getDataSource() {
-    return new DataSource({
-      store: {
-        type: 'odata',
-        version: 2,
-        url: 'https://js.devexpress.com/Demos/SalesViewer/odata/DaySaleDtoes',
-        key: 'Id',
-        beforeSend(request) {
-          const year = new Date().getFullYear() - 1;
-          request.params.startDate = `${year}-05-10`;
-          request.params.endDate = `${year}-5-15`;
-        },
-      },
-    });
   }
 }
